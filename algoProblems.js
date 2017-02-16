50627274ec4d1a872b78aa773ad490337842b4f0
Isaiah Weaver <iswea1@morgan.edu>
2017-02-16 05:32:47 -0500

    factorializeNum fucntion

--- algoProblems.js
+++ algoProblems.js
@@ -11,5 +11,16 @@ function reverseAStirng(str){
 // Factorialize a Number
 
 function factorializeNum (num){
+	// This should apply to all code that is either 0 or 1
+	// I used "===" because these are strictly equal values, meaning they have the same 
+	// value and type
+	if (num === 0 || num === 1)
+		return 1
+	
+	// This for loop is for all numbers not 0 or 1
+	// Just read throught the logic of this solution
+	for (var i = num -1; i >= 1; i--){
+		num = num*i;
+	}
 	return num
 }
