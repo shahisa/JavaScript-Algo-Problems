b1320c78cb3a605b4b9d4d2851d19a96b5634a76
Isaiah Weaver <iswea1@morgan.edu>
2018-02-09 22:28:48 +0100

    linked list

--- pythonProblems.py
+++ pythonProblems.py
@@ -13,3 +13,20 @@ def reverseWords(s):
   for i in words[::-1]:
      revWords.append(i)
   return ' '.join(revWords)
+# Create a singly linked list.
+
+Class Node:
+    def __init__(self,data,nextNode = None):
+        self.data = data
+        self.nextNode = nextNode
+
+    def getData(self):
+        return self.data
+
+    def setData(self,val):
+        self.data = val
+
+    def setNextNode(self,val):
+        self.nextNode = val
+
+Class LinkedList: 
